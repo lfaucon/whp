8f5ef5cfbc70e251300d4cf4cd6d4b2ae3d69fe8
Louis Faucon <lpfaucon@gmail.com>
2018-11-22 23:28:39 +0100

    Add level 5 (#13)

---
+++ level5.js
@@ -0,0 +1,34 @@
+levels.push({
+  robot: {
+    position: [50, 800],
+    hasBlueGun: true,
+    hasYellowGun: true
+  },
+  blocks: [
+    { position: [800, 1000], scale: [50, 5] },
+    { position: [800, 400], scale: [50, 5] }
+  ],
+  killers: [
+    { position: [900, 850], speed: [-250, 0] },
+    { position: [900, 800], speed: [-250, 0] },
+    { position: [900, 600], speed: [-250, 0] },
+    { position: [900, 550], speed: [-250, 0] },
+    { position: [1050, 850], speed: [-250, 0] },
+    { position: [1050, 800], speed: [-250, 0] },
+    { position: [1050, 600], speed: [-250, 0] },
+    { position: [1050, 550], speed: [-250, 0] },
+    { position: [1200, 850], speed: [-250, 0] },
+    { position: [1200, 800], speed: [-250, 0] },
+    { position: [1200, 600], speed: [-250, 0] },
+    { position: [1200, 550], speed: [-250, 0] },
+    { position: [1350, 850], speed: [-250, 0] },
+    { position: [1350, 800], speed: [-250, 0] },
+    { position: [1350, 600], speed: [-250, 0] },
+    { position: [1350, 550], speed: [-250, 0] },
+    { position: [1500, 850], speed: [-250, 0] },
+    { position: [1500, 800], speed: [-250, 0] },
+    { position: [1500, 600], speed: [-250, 0] },
+    { position: [1500, 550], speed: [-250, 0] }
+  ],
+  victory: { position: [1500, 700], scale: [1, 28] }
+});
