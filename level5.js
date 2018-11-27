707550b3fa5e3e47888e4b57e89e774049c6ff44
Louis Faucon <lpfaucon@gmail.com>
2018-11-27 23:49:38 +0100

    Small fix to level 5

--- level5.js
+++ level5.js
@@ -1,34 +1,25 @@
 levels.push({
   robot: {
-    position: [50, 800],
+    position: [50, 600],
     hasBlueGun: true,
     hasYellowGun: true
   },
   blocks: [
-    { position: [800, 1000], scale: [50, 5] },
-    { position: [800, 400], scale: [50, 5] }
+    { position: [800, 800], scale: [50, 3] },
+    { position: [800, 400], scale: [50, 3] }
   ],
   killers: [
-    { position: [900, 850], speed: [-250, 0] },
-    { position: [900, 800], speed: [-250, 0] },
-    { position: [900, 600], speed: [-250, 0] },
-    { position: [900, 550], speed: [-250, 0] },
-    { position: [1050, 850], speed: [-250, 0] },
-    { position: [1050, 800], speed: [-250, 0] },
-    { position: [1050, 600], speed: [-250, 0] },
-    { position: [1050, 550], speed: [-250, 0] },
-    { position: [1200, 850], speed: [-250, 0] },
-    { position: [1200, 800], speed: [-250, 0] },
-    { position: [1200, 600], speed: [-250, 0] },
-    { position: [1200, 550], speed: [-250, 0] },
-    { position: [1350, 850], speed: [-250, 0] },
-    { position: [1350, 800], speed: [-250, 0] },
-    { position: [1350, 600], speed: [-250, 0] },
-    { position: [1350, 550], speed: [-250, 0] },
-    { position: [1500, 850], speed: [-250, 0] },
-    { position: [1500, 800], speed: [-250, 0] },
-    { position: [1500, 600], speed: [-250, 0] },
-    { position: [1500, 550], speed: [-250, 0] }
+    { position: [900, 650], speed: [-250, 0] },
+    { position: [900, 700], speed: [-250, 0] },
+    { position: [1050, 650], speed: [-250, 0] },
+    { position: [1050, 700], speed: [-250, 0] },
+    { position: [1200, 650], speed: [-250, 0] },
+    { position: [1200, 700], speed: [-250, 0] },
+    { position: [1350, 650], speed: [-250, 0] },
+    { position: [1350, 700], speed: [-250, 0] },
+    { position: [1500, 650], speed: [-250, 0] },
+    { position: [1500, 700], speed: [-250, 0] }
   ],
-  victory: { position: [1500, 700], scale: [1, 28] }
+  victory: { position: [1500, 600], scale: [1, 14] },
+  safezones: [{ position: [50, 600], scale: [2, 5] }]
 });
